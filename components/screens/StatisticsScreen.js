c4d07f6e9bf0d62659012cacfd0f3e6799c14dbb
Edgaras <edasedgaras.dir@gmail.com>
2023-12-27 14:21:23 +0200

    Created new files

--- components/screens/StatisticsScreen.js
+++ components/screens/StatisticsScreen.js
@@ -1,56 +1,84 @@
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import { useState, useEffect, useRef } from 'react';
-export default function StatisticsScreen({navigation}) {
-    const [isButtonPressed, setButtonPressed] = useState(false);
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
 
-    const handlePress = () => {
-        setButtonPressed(!isButtonPressed);
-    };
+export default function StatisticsScreen({ navigation }) {
+  const [activeButton, setActiveButton] = useState(null);
 
-    const buttonColor = isButtonPressed ? '#FF5733' : '#191C25';
-    const textColor = isButtonPressed ? '#191C25' : '#BBC4EC';
-    return (
-            <View style={styles.Container}>
-                <View style={styles.topPart}>
-                    <TouchableOpacity
-                    style={[styles.button, { backgroundColor: buttonColor }]}
-                    onPress={handlePress}
-                >
-                    <Text style={[styles.buttonText, { color: textColor }]}>Day</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.button, { backgroundColor: buttonColor }]}
-                    onPress={handlePress}
-                >
-                    <Text style={[styles.buttonText, { color: textColor }]}>Week</Text>
-                </TouchableOpacity>
-                </View>
-            </View>
-    );
+  const handlePress = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  const isDayButtonActive = activeButton === 'day';
+  const isWeekButtonActive = activeButton === 'week';
+
+  return (
+    <View style={styles.Container}>
+      <View style={styles.topPart}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            { backgroundColor: isDayButtonActive ? '#191C25' : '#010101' },
+          ]}
+          onPress={() => handlePress('day')}
+          disabled={isDayButtonActive}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              { color: isDayButtonActive ? '#BBC4EC' : '#FFFFFF' },
+              { fontWeight: isDayButtonActive ? '700' : '400' },
+            ]}
+          >
+            Day
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            { backgroundColor: isWeekButtonActive ? '#191C25' : '#010101' },
+          ]}
+          onPress={() => handlePress('week')}
+          disabled={isWeekButtonActive}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              { color: isWeekButtonActive ? '#BBC4EC' : '#FFFFFF' },
+              { fontWeight: isWeekButtonActive ? '700' : '400' },
+            ]}
+          >
+            Week
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    Container: {
-        backgroundColor: 'black',
-        height: '100%',
-    },
-    topPart: {
-        flexDirection: 'row',
-        alignSelf: 'center',
-    },
-    button: {
-        color: '#BBC4EC',
-        backgroundColor: '#191C25',
-        fontSize: 14,
-        fontWeight: '700',
-        borderRadius: 17,
-    },
-    buttonText: {
-        marginTop: 9,
-        marginBottom: 9,
-        marginLeft: 66,
-        marginRight: 66,
-        textAlign: 'center',
-    },
+  Container: {
+    backgroundColor: 'black',
+    height: '100%',
+  },
+  topPart: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 16,
+  },
+  button: {
+    color: '#BBC4EC',
+    backgroundColor: '#191C25',
+    fontSize: 14,
+    fontWeight: '700',
+    borderRadius: 17,
+    margin: 5,
+  },
+  buttonText: {
+    marginRight: 65,
+    marginLeft: 65,
+    marginTop: 9,
+    marginBottom: 9,
+    textAlign: 'center',
+  },
 });
